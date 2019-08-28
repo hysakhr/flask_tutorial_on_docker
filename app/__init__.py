d009d95af9ca47125f277a90af1e30c9adfd9dcc
hysakhr <hysakhr@gmail.com>
2019-08-28 11:59:23 +0900

    Define and Access the Database

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,11 @@ from flask import Flask
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(
+        __name__,
+        instance_relative_config=True,
+        instance_path='/app/instance'
+    )
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
@@ -28,5 +32,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!!!!!'
+
+    from . import db
+    db.init_app(app)
 
     return app
