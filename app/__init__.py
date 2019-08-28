ff0e1b95f735d00622f63a100ec8c86a39df576b
hysakhr <hysakhr@gmail.com>
2019-08-28 15:07:31 +0900

    Blueprints and Views

--- app/__init__.py
+++ app/__init__.py
@@ -36,4 +36,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    app.add_url_rule('/', endpoint='index')
+
     return app
