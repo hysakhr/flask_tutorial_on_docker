92a9d96699b50489c9482276d2fe460128d62df4
hysakhr <hysakhr@gmail.com>
2019-08-29 23:27:32 +0900

    Fix create post not working

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -14,10 +14,10 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post as p JOIN user as u ON p.author_id = p.id'
+        ' FROM post as p JOIN user as u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    print('blog.index posts : {}'.format(len(posts)))
+
     return render_template('blog/index.html', posts=posts)
 
 
